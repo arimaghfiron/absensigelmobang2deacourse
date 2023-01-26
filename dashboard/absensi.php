55c438aa6a92eba85ba2da2388dc775af67cd240
arimaghfiron <123372105+arimaghfiron@users.noreply.github.com>
2023-01-26 21:49:27 +0700

    Perbaikan fitur pencarian semua data absensi

--- dashboard/absensi.php
+++ dashboard/absensi.php
@@ -5,6 +5,8 @@
   $user_id = $_SESSION['user_id'];
   $tgl = date('Y-m-d');
   $time = date('H:i:s');
+  $intTgl = strtotime($tgl);
+  $stopTgl = $intTgl - 30 * 60 * 60 * 24;
 
   if (isset($_POST['clockout'])) {
     $sql = "UPDATE absensi SET jam_keluar='$time' WHERE user_id='$user_id' AND tgl='$tgl'";
@@ -13,7 +15,17 @@
     } else {
       echo "<p>Maaf terjadi kesalahan</p>";
     }
+
   }
+  
+  $sql = "SELECT * FROM users WHERE user_id='$user_id'";
+  $hasil = $db->query($sql);
+  $datauser = mysqli_fetch_row($hasil);
+
+  if (isset($_POST['semua'])) {
+    $stopTgl = strtotime($datauser[5]);
+  }
+  
   $sql = "SELECT * FROM absensi as a LEFT JOIN request as b ON a.req_id=b.req_id WHERE a.user_id='$user_id' AND a.tgl='$tgl'";
   $result = $db->query($sql);
 
@@ -46,6 +58,26 @@
 <div class="table">
   <div class="table_header">
     <p>DATA ABSENSI</p>
+    <form action="" method="POST">
+
+      <input name="tgl-awal" type="date" class="login-input" value="<?php if(isset($_POST['semua'])){
+                echo date('Y-m-d', $stopTgl);
+            } else if (isset($_POST['tgl-awal'])) {
+                                                                      echo $_POST['tgl-awal'];
+                                                                    } else {
+                                                                      echo date('Y-m-d', $stopTgl);
+                                                                    } ?>">
+      <span style='padding: 0.5rem'>s/d</span>
+      <input name="tgl-akhir" type="date" class="login-input" value="<?php if(isset($_POST['semua'])){
+                echo $tgl;
+            } else if (isset($_POST['tgl-akhir'])) {
+                                                                        echo $_POST['tgl-akhir'];
+                                                                      } else {
+                                                                        echo date('Y-m-d', $intTgl);
+                                                                      } ?>">
+      <button type="submit" name="cari">Cari</button>
+      <button type="submit" name="semua">Semua</button>
+    </form>
   </div>
   <div class="table_section">
     <table class="table">
@@ -62,20 +94,32 @@
         <?php
         $no = 1;
 
-        $intTgl = strtotime($tgl);
-        $stopTgl = $intTgl - 30 * 60 * 60 * 24;
+        $tgl_awal = date('Y-m-d', $stopTgl);
+        $tgl_akhir = $tgl;
 
-        $sql = "SELECT * FROM absensi as a LEFT JOIN request as b ON a.req_id=b.req_id WHERE a.user_id='$user_id' AND a.tgl <= '$tgl' order by tgl desc";
+        if (isset($_POST['cari'])) {
+          if ($_POST['tgl-awal'] != NULL && $_POST['tgl-akhir'] != NULL) {
+            $tgl_awal = $_POST['tgl-awal'];
+            $tgl_akhir = $_POST['tgl-akhir'];
+            $stopTgl = strtotime($tgl_awal);
+            $intTgl = strtotime($tgl_akhir);
+            if ($stopTgl > $intTgl) {
+              header('location:index.php?message=Tanggal Awal melebihi Tanggal Akhir, Mohon masukkan data tanggal dengan benar !');
+              die();
+            }
+          } else {
+            header('location:index.php?message=Data Tanggal KOSONG, Mohon masukkan data tanggal dengan benar ! !');
+            die();
+          }
+        }
+        $sql = "SELECT * FROM absensi as a LEFT JOIN request as b ON a.req_id=b.req_id WHERE a.user_id='$user_id' AND a.tgl BETWEEN '$tgl_awal' AND '$tgl_akhir' ORDER BY a.tgl desc";
+
         $hasil = $db->query($sql);
         $absen = mysqli_fetch_all($hasil);
 
-        $sql = "SELECT * FROM users WHERE user_id='$user_id'";
-        $hasil = $db->query($sql);
-        $datauser = mysqli_fetch_row($hasil);
-
         for ($intTgl; $intTgl >= $stopTgl; $intTgl -= 60 * 60 * 24) {
           $tgljd = date('Y-m-d', $intTgl);
-          
+
           if ($intTgl > strtotime($datauser[5])) {
             echo "<tr class='tr'>";
             echo "<td class='td'>" . $no++ . "</td>";
@@ -83,7 +127,7 @@
 
             $cek = 0;
             foreach ($absen as $data) {
-              
+
               if ($tgljd == $data[2]) {
 
                 echo "<td class='td'> " . $data[3] . " </td>";
--- dashboard/data-absensi.php
+++ dashboard/data-absensi.php
@@ -1,4 +1,11 @@
 <?php
+include("../connection.php");
+date_default_timezone_set("Asia/Jakarta");
+$tgl = date('Y-m-d');
+
+$intTgl = strtotime($tgl);
+$stopTgl = $intTgl - 30 * 60 * 60 * 24;
+
 if (
     !isset($_GET['submenu']) ||
     $_GET['submenu'] !== "input-absen" &&
@@ -8,6 +15,17 @@ if (
     include($_GET['submenu'] . ".php");
 }
 
+$sqluser = "SELECT * FROM users";
+$hasil = $db->query($sqluser);
+$user = mysqli_fetch_all($hasil);
+
+if (isset($_POST['semua'])) {
+    foreach ($user as $minuser) {
+        if ($stopTgl > strtotime($minuser[5])) {
+            $stopTgl = strtotime($minuser[5]);
+        }
+    }
+}
 ?>
 <div class="table">
 
@@ -15,9 +33,26 @@ if (
 
         <p>DATA ABSENSI KARYAWAN</p>
         <form action="" method="POST">
-            <input placeholder=" NIK" name="NIK" type="text" class="login-input" value="<?php if (isset($_POST['NIK'])) {
-                                                                                            echo $_POST['NIK'];
-                                                                                        } ?>">
+
+            <input name="tgl-awal" type="date" class="login-input" value="<?php if(isset($_POST['semua'])){
+                echo date('Y-m-d', $stopTgl);
+            } else if (isset($_POST['tgl-awal'])) {
+                                                                                echo $_POST['tgl-awal'];
+                                                                            } else {
+                                                                                echo date('Y-m-d', $stopTgl);
+                                                                            } ?>">
+            <span style='padding: 0.5rem'>s/d</span>
+            <input name="tgl-akhir" type="date" class="login-input" value="<?php 
+            if(isset($_POST['semua'])){
+                echo $tgl;
+            } else if (isset($_POST['tgl-akhir'])) {
+            echo $_POST['tgl-akhir'];
+        } else {
+            echo date('Y-m-d', $intTgl);
+        } ?>">
+            <input placeholder=" NIK/Nama/Jabatan" name="NIK" type="text" class="login-input" value="<?php if (isset($_POST['NIK'])) {
+                                                                                                            echo $_POST['NIK'];
+                                                                                                        } ?>">
             <button type="submit" name="cari">Cari</button>
             <button type="submit" name="semua">Semua</button>
         </form>
@@ -40,47 +75,62 @@ if (
             <tbody>
                 <?php
 
-                include("../connection.php");
-                date_default_timezone_set("Asia/Jakarta");
-                $tgl = date('Y-m-d');
-                $sql = "SELECT * FROM `users` as a,`absensi` as b LEFT JOIN request as c ON b.req_id=c.req_id WHERE a.user_id=b.user_id AND b.tgl <= '$tgl'";
-                if (isset($_POST['cari']) && $_POST['NIK'] != NULL) {
-                    $NIK = $_POST['NIK'];
-                    $sql = $sql . " AND a.user_id='$NIK'";
+                $tgl_awal = date('Y-m-d', $stopTgl);
+                $tgl_akhir = $tgl;
+                $sql = "SELECT * FROM `users` as a,`absensi` as b LEFT JOIN request as c ON b.req_id=c.req_id WHERE a.user_id=b.user_id";
+
+                if (isset($_POST['cari'])) {
+                    if ($_POST['NIK'] != NULL) {
+                        $NIK = $_POST['NIK'];
+                        $sql = $sql . " AND a.user_id='$NIK'";
+                        $sqluser = $sqluser . " WHERE user_id='$NIK' OR nama_lengkap LIKE '%$NIK%' OR role LIKE '%$NIK%'";
+                    }
+                    if ($_POST['tgl-awal'] != NULL && $_POST['tgl-akhir'] != NULL) {
+                        $tgl_awal = $_POST['tgl-awal'];
+                        $tgl_akhir = $_POST['tgl-akhir'];
+                        $stopTgl = strtotime($tgl_awal);
+                        $intTgl = strtotime($tgl_akhir);
+                        if ($stopTgl > $intTgl) {
+                            header('location:index-admin.php?menu=data-absensi&message=Tanggal Awal melebihi Tanggal Akhir, Mohon masukkan data tanggal dengan benar !');
+                            die();
+                        }
+                    } else {
+                        header('location:index-admin.php?menu=data-absensi&message=Data Tanggal KOSONG, Mohon masukkan data tanggal dengan benar ! !');
+                        die();
+                    }
                 }
-                $sql = $sql . " ORDER BY b.tgl desc, a.user_id";
-                $sqluser = "SELECT * FROM users";
-                if (isset($_POST['cari']) && $_POST['NIK'] != NULL) {
-                    $NIKuser = $_POST['NIK'];
-                    $sqluser = $sqluser . " WHERE user_id='$NIKuser'";
-                }
+
+                $sql = $sql . " AND b.tgl BETWEEN '$tgl_awal' AND '$tgl_akhir' ORDER BY b.tgl desc, a.user_id";
+
                 $hasil = $db->query($sql);
                 $absen = mysqli_fetch_all($hasil);
-        
-                $hasil = $db->query($sqluser);
-                $user = mysqli_fetch_all($hasil);
 
                 $no = 1;
-
-                $intTgl = strtotime($tgl);
-                $stopTgl = $intTgl - 30 * 60 * 60 * 24;
-                
 
                 for ($intTgl; $intTgl >= $stopTgl; $intTgl -= 60 * 60 * 24) {
                     $tgljd = date('Y-m-d', $intTgl);
 
                     foreach ($user as $datauser) {
-                        if($intTgl > strtotime($datauser[5])){
-           
+
+                        $tglkeluar = strtotime($datauser[6]);
+                        if ($tglkeluar == false) {
+                            $tglkeluar = $intTgl + 60 * 60 * 24;
+                        }
+
+                        if ($intTgl > strtotime($datauser[5]) && $intTgl < $tglkeluar) {
+
+
                             echo "<tr class='tr'>";
                             echo "<td class='td'>" . $no++ . "</td>";
                             echo "<td class='td'>" . $datauser[1] . "</td>";
                             echo "<td class='td'>" . $datauser[3] . "</td>";
                             echo "<td class='td'>" . $datauser[4] . "</td>";
                             echo "<td class='td'> " . $tgljd . " </td>";
-    
+
+
                             $cek = 0;
-                                foreach ($absen as $data) {
+                            foreach ($absen as $data) {
+
                                 if ($tgljd == $data[9] && $datauser[1] == $data[1]) {
                                     echo "<td class='td'>" . $data[10] . "</td>";
                                     echo "<td class='td'>" . $data[11] . "</td>";
@@ -112,7 +162,7 @@ if (
                                 }
                             }
                             echo "</tr>";
-                        } 
+                        }
                     }
                 }
                 ?>
